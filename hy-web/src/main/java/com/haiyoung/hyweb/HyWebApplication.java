6254395eaaa3ca9e21abac5e709d0635e4bc7da1
haiyoung <haiyoungshao@126.com>
2017-11-08 10:37:29 +0800

    add requestMapping for home

--- hy-web/src/main/java/com/haiyoung/hyweb/HyWebApplication.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/HyWebApplication.java
@@ -2,9 +2,21 @@ package com.haiyoung.hyweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class HyWebApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HyWebApplication.class, args);
