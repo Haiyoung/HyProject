9daf3eb55a23c24492efecb4c3f3c21dbc8bab2f
Haiyoung <haiyoungshao@126.com>
2017-11-12 19:55:38 +0800

    add login.html

--- hy-web/src/main/java/com/haiyoung/hyweb/web/controller/LoginController.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/web/controller/LoginController.java
@@ -3,8 +3,15 @@ package com.haiyoung.hyweb.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/page")
 public class LoginController{
-    
+
+    @RequestMapping("/login")
+    private String login(Map<String,Object> map){
+
+        return "login";
+    }
 }
