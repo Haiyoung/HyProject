214eea9d1135b6082deee260154da93b6985239b
haiyoung <haiyoungshao@126.com>
2017-11-17 19:14:13 +0800

    add file loader relate

--- hy-web/src/main/java/com/haiyoung/hyweb/web/controller/FileLoaderController.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/web/controller/FileLoaderController.java
@@ -4,7 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Map;
 
 @Controller
@@ -17,5 +23,36 @@ public class FileLoaderController{
     public String fileupload(Map<String,Object> map){
 
         return "fileupload";
+    }
+
+
+    @RequestMapping("/doUpload")
+    @ResponseBody
+    public String uploading(HttpServletRequest request, @RequestParam("file") MultipartFile file){
+
+        if(!file.isEmpty()){
+            try {
+//                String filePath = request.getSession().getServletContext().getRealPath("file/");
+                String filePath = "./file/";
+                File targetFile = new File(filePath);
+                if(!targetFile.exists()){
+                    targetFile.mkdirs();
+                }
+                FileOutputStream out = new FileOutputStream(filePath+file.getOriginalFilename());
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+                System.out.println(filePath);
+                return "uploading success!";
+
+            }catch (Exception e){
+                log.error("uploading failure!");
+                return "uploading failure!";
+            }
+        }else{
+            return "uploading failure!";
+        }
+
+
     }
 }
