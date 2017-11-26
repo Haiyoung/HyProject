63dc8cf9c3ef6a3be82eb3e0984d22249fa54347
Haiyoung <haiyoungshao@126.com>
2017-11-26 18:12:22 +0800

    Reconstruction File Upload

--- hy-web/src/main/java/com/haiyoung/hyweb/biz/file/FileService.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/biz/file/FileService.java
@@ -1,8 +1,20 @@
 package com.haiyoung.hyweb.biz.file;
 
+import com.haiyoung.hyweb.util.StringUtils;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import java.io.File;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,12 +23,19 @@ import java.util.List;
 @Service
 public class FileService {
 
-    /*获取目录下所有文件*/
-    public static List<File> getFiles(String realpath, List<File> files) {
-        File realFile = new File(realpath);
+    private static final Logger log = LoggerFactory.getLogger(FileService.class);
+
+    /**
+     * 获取目录下所有文件
+     * @param realPath
+     * @param files
+     * @return
+     */
+    public List<File> getFiles(String realPath, List<File> files) {
+        File realFile = new File(realPath);
         if (realFile.isDirectory()) {
-            File[] subfiles = realFile.listFiles();
-            for (File file : subfiles) {
+            File[] subFiles = realFile.listFiles();
+            for (File file : subFiles) {
                 if (file.isDirectory()) {
                     getFiles(file.getAbsolutePath(), files);
                 } else {
@@ -25,6 +44,119 @@ public class FileService {
             }
         }
         return files;
+    }
+
+    /**
+     * 获取文件的绝对路径
+     * @param files
+     * @return
+     */
+    public List<String> getFilePath(List<File> files){
+        List<String> pathList = new ArrayList<>();
+        if(files != null && !files.isEmpty()){
+            for(File file:files){
+                if(file.exists()){
+                    pathList.add(file.getAbsolutePath());
+                }
+            }
+        }
+        return pathList;
+    }
+
+    /**
+     * 单文件上传
+     * @param filePath
+     * @param file
+     * @return
+     */
+    public String fileUpload(String filePath, MultipartFile file){
+
+        if(file !=null && !file.isEmpty()){
+            if(StringUtils.isEmpty(filePath)){
+                filePath = "./file/";
+            }
+            try {
+                File targetFile = new File(filePath);
+                if(!targetFile.exists()){
+                    targetFile.mkdirs();
+                }
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filePath+file.getOriginalFilename()));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+                return "uploading success!";
+            }catch (Exception e){
+                log.error("uploading failure!");
+                return "uploading failure!";
+            }
+
+        }else{
+            return "file is not exist!";
+        }
+
+    }
+
+    /**
+     * 多文件上传
+     * @param request
+     * @param filePath
+     * @return
+     */
+    public String multiFileUpload(HttpServletRequest request,String filePath){
+        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("file");
+        if(files != null && !files.isEmpty()){
+            for(MultipartFile file:files){
+                fileUpload(filePath, file);
+            }
+            return "uploading success!";
+        }else{
+            return "files are not exist!";
+        }
+    }
+
+    /**
+     * 文件下载
+     * @param fileAbPath 文件的绝对路径
+     * @return
+     * @throws IOException
+     */
+    public ResponseEntity<byte[]> fileDownload(String fileAbPath) throws IOException {
+        if(StringUtils.isEmpty(fileAbPath)){
+            fileAbPath = "./file/linux.txt";
+        }
+        File file = new File(fileAbPath);
+        HttpHeaders headers = new HttpHeaders();
+        String[] temp = fileAbPath.split("/");
+        String fileName = new String(temp[temp.length-1].getBytes("UTF-8"),"iso-8859-1");
+        headers.setContentDispositionFormData("attachment",fileName);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file), headers, HttpStatus.CREATED);
+    }
+
+    /**
+     *文件下载
+     * @param request
+     * @param fileAbPath
+     * @return
+     * @throws IOException
+     */
+    public ResponseEntity<byte[]> fileDownload(HttpServletRequest request, String fileAbPath) throws IOException{
+        if(StringUtils.isEmpty(fileAbPath)){
+            fileAbPath = "./file/linux.txt";
+        }
+        byte[] body = null;
+        String[] temp = fileAbPath.split("/");
+        File file = new File(fileAbPath);
+        InputStream in = new FileInputStream(fileAbPath);
+        body = new byte[in.available()];
+        in.read(body);
+        HttpHeaders headers = new HttpHeaders();
+        String fileName = new String(temp[temp.length-1].getBytes("UTF-8"),"iso-8859-1");
+        headers.add("Content-Disposition", "attachment;filename=" + fileName);
+        HttpStatus statusCode = HttpStatus.OK;
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+        in.close();
+        return response;
     }
 
 }
--- hy-web/src/main/java/com/haiyoung/hyweb/web/controller/FileLoaderController.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/web/controller/FileLoaderController.java
@@ -20,6 +20,7 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -43,122 +44,67 @@ public class FileLoaderController{
     @ResponseBody
     public String uploading(HttpServletRequest request, @RequestParam("file") MultipartFile file){
 
-        if(!file.isEmpty()){
-            try {
-//                String filePath = request.getSession().getServletContext().getRealPath("file/");
-                String filePath = "./file/";
-                File targetFile = new File(filePath);
-                if(!targetFile.exists()){
-                    targetFile.mkdirs();
-                }
-                FileOutputStream out = new FileOutputStream(filePath+file.getOriginalFilename());
-                out.write(file.getBytes());
-                out.flush();
-                out.close();
-                System.out.println(filePath);
-                return "uploading success!";
-
-            }catch (Exception e){
-                log.error("uploading failure!");
-                return "uploading failure!";
-            }
+        if (file != null && !file.isEmpty()){
+            fileService.fileUpload(null, file);
+            return "single file upload success!";
         }else{
-            return "uploading failure!";
+            return "file is not exist!";
         }
-
 
     }
 
     @RequestMapping("/doMultiUpload")
     @ResponseBody
     public String multiUploading(HttpServletRequest request){
-        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("file");
-//        MultipartFile file = null;
-//        FileOutputStream out = null;
-        if(files != null && !files.isEmpty()){
-            for(MultipartFile file:files){
-                if(!file.isEmpty()){
-                    try {
-//                String filePath = request.getSession().getServletContext().getRealPath("file/");
-                        String filePath = "./file/";
-                        File targetFile = new File(filePath);
-                        if(!targetFile.exists()){
-                            targetFile.mkdirs();
-                        }
-                        FileOutputStream out = new FileOutputStream(filePath+file.getOriginalFilename());
-                        out.write(file.getBytes());
-                        out.flush();
-                        out.close();
-                        System.out.println(filePath);
 
-                    }catch (Exception e){
-                        log.error("uploading failure!");
-                    }
-                }
-            }
-            return "multiUploading success!";
-        }else{
-            return "multiUploading failure!";
-        }
+        fileService.multiFileUpload(request,"E:/图片/");
+        return "multiFile upload success!";
 
     }
 
     /*文件下载*/
     @RequestMapping("/fileDownload")
     public ResponseEntity<byte[]> download() throws IOException{
-        String path = "./file/linux.txt";
-        File file = new File(path);
-        HttpHeaders headers = new HttpHeaders();
-        String fileName = new String("download-linux.txt".getBytes("UTF-8"),"iso-8859-1");
-        headers.setContentDispositionFormData("attachment",fileName);
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers, HttpStatus.CREATED);
+        return fileService.fileDownload("E:/图片/邵海洋.jpg");
     }
 
     //用ResponseEntity<byte[]> 返回值完成文件下载
     @RequestMapping("/fileDown")
     public ResponseEntity<byte[]> fileDownload(HttpServletRequest request, @RequestParam(value = "path",required = false) String path)
             throws Exception {
-        byte[] body = null;
-        // ServletContext servletContext = request.getServletContext();
-        String fileName = "邵海洋.jpg"; //从uuid_name.jpg中截取文件名
-        // String path = servletContext.getRealPath("/WEB-INF/res/" + fileName);
-//        path = "E:\\图片\\姐姐_20170907223722.jpg";
-//        path = "E:\\\\sql\\\\"+path;
-        File file = new File(path);
-        InputStream in = new FileInputStream(file);
-        body = new byte[in.available()];
-        in.read(body);
-        HttpHeaders headers = new HttpHeaders();
-        fileName = new String(fileName.getBytes("gbk"), "iso8859-1");
-        headers.add("Content-Disposition", "attachment;filename=" + fileName);
-        HttpStatus statusCode = HttpStatus.OK;
-        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
-        in.close();
-        return response;
+//        path = "E:\\图片\\"+path;
+        path = request.getParameter("path");
+        return fileService.fileDownload(request, path);
     }
 
     //文件列表的显示
     /*https://www.cnblogs.com/lixiang1993/p/7443246.html*/
     @RequestMapping(value = "/showFile")
     public String showFile(HttpServletRequest request,Map<String,Object> map){
-//        ServletContext servletContext = request.getServletContext();
-//        String path=servletContext.getRealPath("E://图片");
-        File file = new File("/opt");
+        File file = new File("E:/图片");
         File[] fileList = null;
         if(file.exists()){
             fileList = file.listFiles();
         }
-//        File[] fileList = new File(path).listFiles();
-//        m.addAttribute("fileList", fileList);
+
         map.put("fileList",fileList);
         if(fileList.length > 0){
             for(File obj:fileList){
                 System.out.println(obj.getName());
+                System.out.println(obj.getAbsolutePath());
             }
         }
 
-//        System.out.println(fileList.toString());
+        System.out.println(fileList.toString());
         return "showFile";
+    }
+
+    @RequestMapping(value = "filesPath")
+    @ResponseBody
+    public List<String> getFilePath(){
+        String path = "E:/图片/";
+        List<File> files = new ArrayList<>();
+        files = fileService.getFiles(path, files);
+        return fileService.getFilePath(files);
     }
 }
