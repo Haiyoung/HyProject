f44877e4cee88d2d9760105ed12215bd0b3a5b8f
haiyoung <haiyoungshao@126.com>
2017-11-28 18:33:46 +0800

    add @RunWith and @SpringBootTest annotation

--- hy-web/src/test/java/com/haiyoung/hyweb/TestRedis.java
+++ hy-web/src/test/java/com/haiyoung/hyweb/TestRedis.java
@@ -2,10 +2,15 @@ package com.haiyoung.hyweb;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestRedis {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
