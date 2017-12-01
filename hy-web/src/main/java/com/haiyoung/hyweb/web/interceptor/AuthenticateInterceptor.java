8ed983c93a9556b41b32dd7f3d76fae4f29e2a2e
haiyoung <haiyoungshao@126.com>
2017-12-01 09:02:34 +0800

    add authenticate interceptor

---
+++ hy-web/src/main/java/com/haiyoung/hyweb/web/interceptor/AuthenticateInterceptor.java
@@ -0,0 +1,16 @@
+package com.haiyoung.hyweb.web.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenticateInterceptor extends HandlerInterceptorAdapter{
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("ServletPath:"+request.getServletPath());
+        return true;
+    }
+}
