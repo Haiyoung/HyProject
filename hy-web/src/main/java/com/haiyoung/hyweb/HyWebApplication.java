53046b2a99f59a0f85c0f7fac080fbc2fc55940c
haiyoung <haiyoungshao@126.com>
2017-12-08 11:39:25 +0800

    add websocket test

--- hy-web/src/main/java/com/haiyoung/hyweb/HyWebApplication.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/HyWebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +25,14 @@ public class HyWebApplication {
 
 	public static void main(String[] args) {
 //		SpringApplication.run(HyWebApplication.class, args);
-		SpringApplication application = new SpringApplication(HyWebApplication.class);
+/*		SpringApplication application = new SpringApplication(HyWebApplication.class);
 //		application.setBannerMode(Banner.Mode.OFF);
-		application.run(args);
+		*//*获取所有启动时注入的bean*//*
+		application.run(args);*/
+		ApplicationContext ctx = SpringApplication.run(HyWebApplication.class, args);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		for(String item:beanNames){
+			System.out.println(item);
+		}
 	}
 }
