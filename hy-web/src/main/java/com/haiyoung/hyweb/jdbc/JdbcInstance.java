a21c1d5be139495eba1d4fde501f52e4bd0d0343
Haiyoung <haiyoungshao@126.com>
2017-12-14 22:00:39 +0800

    add serverTimezone setting of mysql server

--- hy-web/src/main/java/com/haiyoung/hyweb/jdbc/JdbcInstance.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/jdbc/JdbcInstance.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class JdbcInstance {
 
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/haiyoung?useSSL=true";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/haiyoung?useSSL=true&serverTimezone=UTC";
     private static final String userName = "root";
     private static final String passwd = "123456";
 
