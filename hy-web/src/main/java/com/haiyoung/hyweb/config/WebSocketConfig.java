4b9d95acd360c2bc42af8817d8ed04bc913663d3
haiyoung <haiyoungshao@126.com>
2017-12-20 17:57:40 +0800

    add ...

--- hy-web/src/main/java/com/haiyoung/hyweb/config/WebSocketConfig.java
+++ hy-web/src/main/java/com/haiyoung/hyweb/config/WebSocketConfig.java
@@ -2,12 +2,14 @@ package com.haiyoung.hyweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-//@EnableWebSocket
-public class WebSocketConfig {
+@EnableWebSocket
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
     /**
      * 首先要注入ServerEndpointExporter，这个bean会自动注册使用了
      * @ServerEndpoint注解声明的Websocket endpoint。要注意，如果
@@ -15,8 +17,19 @@ public class WebSocketConfig {
      * 就不要注入ServerEndpointExporter，因为它将由容器自己提供和管理。
      *
      */
-/*    @Bean
+    @Bean
     public ServerEndpointExporter serverEndpointExporter(){
         return new ServerEndpointExporter();
-    }*/
+    }
+
+    /**
+     *The registerStompEndpoints() method registers the "/websocket" endpoint,
+     *enabling SockJS fallback options so that alternate transports may be used if WebSocket
+     *is not available. The SockJS client will attempt to connect to "/websocket" and
+     *use the best transport available (websocket, xhr-streaming, xhr-polling, etc).
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/websocket").withSockJS();
+    }
 }
