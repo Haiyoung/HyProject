44b69e05872eff3841709b48e8817b6219b509c3
SHY <haiyang.shao1@oyohotels.cn>
2018-12-16 22:17:48 +0800

    string converter for string trim

---
+++ oyotest/src/main/java/com/haiyoung/oyotest/controller/StringTrimTestController.java
@@ -0,0 +1,20 @@
+package com.haiyoung.oyotest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Haiyang
+ * @Date: 2018/12/16 下午9:59
+ */
+
+@RestController
+public class StringTrimTestController {
+
+    @GetMapping(value = "/stringStrim")
+    public String stringTrim(@RequestParam(value = "string") String string){
+        return string;
+    }
+}
