bf602289e1a0a19261e5b22652120f9fc46acbcd
SHY <haiyang.shao1@oyohotels.cn>
2019-01-10 10:39:41 +0800

    add sharding-jdbc test

---
+++ oyotest/src/main/java/com/haiyoung/oyotest/config/DataSourceConfig.java
@@ -0,0 +1,9 @@
+package com.haiyoung.oyotest.config;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+
+
+}
---
+++ oyotest/src/test/java/com/haiyoung/oyotest/ShardingJdbcTest.java
@@ -0,0 +1,17 @@
+package com.haiyoung.oyotest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShardingJdbcTest {
+
+    @Test
+    public void insertData(){
+
+
+    }
+}
