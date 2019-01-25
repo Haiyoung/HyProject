8c3a82fccd55a5e2dc32914c94f6a9c40d4ffd5a
SHY <haiyoungshao@126.com>
2019-01-25 18:32:59 +0800

    修改logback配置文件

--- oyotest/src/main/java/com/haiyoung/oyotest/controller/StringTrimTestController.java
+++ oyotest/src/main/java/com/haiyoung/oyotest/controller/StringTrimTestController.java
@@ -1,5 +1,6 @@
 package com.haiyoung.oyotest.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2018/12/16 下午9:59
  */
 
+@Slf4j
 @RestController
 public class StringTrimTestController {
 
@@ -20,6 +22,9 @@ public class StringTrimTestController {
 
     @GetMapping(value = "/ping")
     public String ping(){
+        log.info("info");
+        log.debug("debug");
+        log.error("error");
         return "pong";
     }
 }
