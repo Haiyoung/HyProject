cb000d30cde9992fad9d3875484b13b2bdac5d6e
SHY <haiyoungshao@126.com>
2019-04-17 16:25:21 +0800

    add tiny code module

---
+++ tiny-code/src/main/java/com/haiyoung/tinycode/TinyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.haiyoung.tinycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyCodeApplication.class, args);
+    }
+
+}
---
+++ tiny-code/src/test/java/com/haiyoung/tinycode/TinyCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haiyoung.tinycode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinyCodeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
